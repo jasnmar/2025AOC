2b921d55221956f00a949b2d5250e004e8ffd663
Jason Martin <jasnmar@gmail.com>
2025-12-03 11:51:02 -0500

    Problem 1

--- 03/index.ts
+++ 03/index.ts
@@ -1,7 +1,76 @@
+import * as fs from "fs"
+
+const FILENAME = "./03/sample.txt"
+
+function convertBatteryStringToNumberArray(batteryString: string): number[] {
+  const batteryDigits = batteryString.split("")
+  const batteryValues = batteryDigits.map((batteryDigit) => {
+    return parseInt(batteryDigit)
+  })
+  return batteryValues
+}
+
+function findLargestNumberInArray(valueArray: number[]): number {
+  const largest = Math.max(...valueArray)
+  // console.log("largest: ", largest)
+  return largest
+}
 
 
-function main() {
-  console.log("running")
+
+
+
+function one() {
+  // console.log("running")
+
+  const fileContents: string = fs.readFileSync(FILENAME, "utf8")
+  if (fileContents.length > 0) {
+    // console.log(fileContents)
+    //Let's make an array out of each line, which represents a single battery
+    const batteriesArray = fileContents.split(/\r?\n/)
+    const batterySelection = batteriesArray.map((battery) => {
+      const batteryValueArray = convertBatteryStringToNumberArray(battery)
+      const largest = findLargestNumberInArray(batteryValueArray)
+      const largestCount = batteryValueArray.filter(
+        (batteryVal) => batteryVal === largest
+      )
+      if (largestCount.length > 1) {
+        // console.log("Returning early with repeating numbers ", largestCount)
+        return [largestCount[0], largestCount[0]]
+      }
+      //We didn't have 2 of the largest battery, so we need to find the next largest
+      //Find the next largest before the index of the largest and after the index of the
+      //largest then see which is better
+      const largestIndex = batteryValueArray.findIndex(
+        (value) => value === largest
+      )
+
+      const afterBatteryValueArray = batteryValueArray.slice(largestIndex + 1)
+      const beforeBatteryValueArray = batteryValueArray.slice(0, largestIndex)
+      const secondLargestAfter = findLargestNumberInArray(
+        afterBatteryValueArray
+      )
+      const secondLargestBefore = findLargestNumberInArray(
+        beforeBatteryValueArray
+      )
+      const valueWithBefore = parseInt(secondLargestBefore.toString() + largest)
+      const valueWithAfter = parseInt(largest.toString() + secondLargestAfter)
+      if (valueWithBefore > valueWithAfter) {
+        return [secondLargestBefore, largest]
+      } else {
+        return [largest, secondLargestAfter]
+      }
+    })
+    console.log("batterySelection: ", JSON.stringify(batterySelection))
+    const batteryArray = batterySelection.map((battery) => {
+      if (battery[0] && battery[1]) {
+        return parseInt(battery[0]?.toString() + battery[1]?.toString())
+      }
+    })
+    console.log("batteryArray: ", batteryArray)
+    const total = batteryArray.reduce((acc: number, num: number | undefined) => acc + (num || 0), 0)
+    console.log("total: ", total);
+  }
 }
 
 main()
