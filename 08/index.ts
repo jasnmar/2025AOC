4c71b9600d58f9c6a504c07753bdbac9f8a2059a
Jason Martin <jasnmar@gmail.com>
2025-12-09 15:27:35 -0500

    Day 8 part 1

---
+++ 08/index.ts
@@ -0,0 +1,159 @@
+import * as fs from "fs"
+
+const FILENAME = "./08/sample.txt"
+const CONNECTIONS = 10
+function convertToCoordinates(arr: string[]) {
+  let retArr: [number, number, number][] = []
+  const intArr = arr.map((coordinateSet) => {
+    const cordArr = coordinateSet.split(",")
+    const coords = cordArr.map((cordString) => {
+      return parseInt(cordString)
+    })
+    return [coords[0] || 0, coords[1] || 0, coords[2] || 0]
+  }) as [number, number, number][]
+  retArr = intArr
+  return retArr
+}
+function computeDistanceBetweenCoordinates(
+  coords1: [number, number, number],
+  coords2: [number, number, number]
+) {
+  const deltaX = coords1[0] - coords2[0]
+  const deltaY = coords1[1] - coords2[1]
+  const deltaZ = coords1[2] - coords2[2]
+  return Math.sqrt(deltaX ** 2 + deltaY ** 2 + deltaZ ** 2)
+}
+function removeDuplicates(arr: number[]): number[] {
+  const retArr: number[] = [...new Set(arr)]
+  retArr.sort((a, b) => a - b)
+  return retArr
+}
+function makeCoordinateMap(coordinateArray: [number, number, number][]) {
+  const distanceArray: [number, number, number][] = []
+  for (let i = 0; i < coordinateArray.length; i++) {
+    for (let j = i + 1; j < coordinateArray.length; j++) {
+      const coord1 = coordinateArray[i]
+      const coord2 = coordinateArray[j]
+      if (coord1 && coord2) {
+        const distance = computeDistanceBetweenCoordinates(coord1, coord2)
+        distanceArray.push([distance, i, j])
+      }
+    }
+  }
+  return distanceArray
+}
+
+function checkForBox(nextBox1: number, circuitMap: [number, number][]): number {
+  let foundIndex = -1
+  foundIndex = circuitMap.findIndex((circuitArray) => {
+    const result = circuitArray.findIndex((box) => box === nextBox1)
+    return result === -1 ? false : true
+  })
+  return foundIndex
+}
+
+function mergeCircuits(
+  circuitIndex1: number,
+  circuitIndex2: number,
+  circuitMap: number[][]
+) {
+  const firstCircuit: number[] | undefined = circuitMap[circuitIndex1]
+  const secondCircuit: number[] | undefined = circuitMap[circuitIndex2]
+  if (firstCircuit && secondCircuit) {
+    const combinedCircuit = removeDuplicates([
+      ...firstCircuit,
+      ...secondCircuit,
+    ])
+
+    circuitMap[circuitIndex1] = combinedCircuit as [number, number]
+    circuitMap.splice(circuitIndex2, 1)
+  }
+  if (circuitMap.length === 1) {
+    console.log("circuitMap 000000: ", circuitMap)
+  }
+  return circuitMap
+}
+
+function connectCircuits(arr: [number, number, number][]) {
+  if (arr.length === 0) {
+    return []
+  }
+  let currentConnections = 0
+  const circuitMap: [number, number][] = []
+  if (arr[0] !== undefined) {
+    const firstElem = arr[0][1] as number
+    const secondElem = arr[0][2] as number
+    circuitMap.push([firstElem, secondElem])
+    for (let i = 1; i < CONNECTIONS; i++) {
+      const currentArrElement = arr[i]
+      if (currentArrElement) {
+        const val1 = currentArrElement[1]
+        const val2 = currentArrElement[2]
+        const existingCircuitIndex1 = checkForBox(val1, circuitMap)
+        const existingCircuitIndex2 = checkForBox(val2, circuitMap)
+        if (existingCircuitIndex1 === -1 && existingCircuitIndex2 === -1) {
+          circuitMap.push([val1, val2])
+        } else if (
+          existingCircuitIndex1 > -1 &&
+          existingCircuitIndex2 > -1 &&
+          existingCircuitIndex1 !== existingCircuitIndex2
+        ) {
+          mergeCircuits(
+            existingCircuitIndex1,
+            existingCircuitIndex2,
+            circuitMap
+          )
+        } else {
+          const circuitIndex =
+            existingCircuitIndex1 > existingCircuitIndex2
+              ? existingCircuitIndex1
+              : existingCircuitIndex2
+          const existingValue = circuitMap[circuitIndex]
+          if (existingValue) {
+            existingValue.push(val1)
+            existingValue.push(val2)
+            circuitMap[circuitIndex] = removeDuplicates(existingValue) as [
+              number,
+              number
+            ]
+          }
+        }
+
+        console.log("circutMap: ", JSON.stringify(circuitMap))
+      }
+    }
+  }
+  return circuitMap
+}
+
+function main(): void {
+  const fileContents: string = fs.readFileSync(FILENAME, "utf8")
+  if (fileContents.length > 0) {
+    const data = fileContents.split(/\r?\n/)
+
+    const coordinates = convertToCoordinates(data)
+    console.log("coordinates: ", JSON.stringify(coordinates))
+    const distances = makeCoordinateMap(coordinates)
+    distances.sort((a, b) => {
+      const val1 = a[0]
+      const val2 = b[0]
+      return (val1 as number) - (val2 as number)
+    })
+    console.log("distances: ", JSON.stringify(distances))
+
+    const circuitsMap = connectCircuits(distances)
+    console.log("circuitsMap: ", JSON.stringify(circuitsMap))
+    const circuitCounts = circuitsMap.map((circuit) => {
+      return circuit.length
+    })
+    circuitCounts.sort((a, b) => b - a)
+    const best3 = circuitCounts.slice(0, 3)
+    console.log("best3: ", best3)
+    const total = best3.reduce((acc, num) => {
+      return acc * num
+    }, 1)
+    console.log("total: ", total)
+  }
+}
+
+main()
